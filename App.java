fd98927ac65cdc34c69caf7893c058a484c0e7a8
Ajaffer5 <149440888+Ajaffer5@users.noreply.github.com>
2024-03-21 12:59:35 -0400

    Add files via upload

--- App.java
+++ App.java
@@ -1,59 +1,77 @@
-class Animal {
-    public int age;
-    public String gender;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
-    public boolean isMammal() {
-        System.out.println("This is the isMammal method from class Animal");
-        
-        return true; 
-    }
+public class App extends Application {
 
-    public void mate() {
-        System.out.println("This is the mate method from class Animal");
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Metric Converter");
+
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Label kmLabel = new Label("Kilometers:");
+        grid.add(kmLabel, 0, 0);
+
+        TextField kmTextField = new TextField();
+        grid.add(kmTextField, 1, 0);
+
+        Button convertKmToMilesBtn = new Button("Convert to Miles");
+        grid.add(convertKmToMilesBtn, 2, 0);
+
+        Label milesLabel = new Label("Miles:");
+        grid.add(milesLabel, 0, 1);
+
+        TextField milesTextField = new TextField();
+        milesTextField.setEditable(false);
+        grid.add(milesTextField, 1, 1);
+
+        convertKmToMilesBtn.setOnAction(e -> {
+            double kilometers = Double.parseDouble(kmTextField.getText());
+            double miles = kilometers * 0.621371;
+            milesTextField.setText(String.format("%.2f", miles));
+        });
+
+        Label kgLabel = new Label("Kilograms:");
+        grid.add(kgLabel, 0, 2);
+
+        TextField kgTextField = new TextField();
+        grid.add(kgTextField, 1, 2);
+
+        Button convertKgToPoundsBtn = new Button("Convert to Pounds");
+        grid.add(convertKgToPoundsBtn, 2, 2);
+
+        Label poundsLabel = new Label("Pounds:");
+        grid.add(poundsLabel, 0, 3);
+
+        TextField poundsTextField = new TextField();
+        poundsTextField.setEditable(false);
+        grid.add(poundsTextField, 1, 3);
+
+        convertKgToPoundsBtn.setOnAction(e -> {
+            double kilograms = Double.parseDouble(kgTextField.getText());
+            double pounds = kilograms * 2.20462;
+            poundsTextField.setText(String.format("%.2f", pounds));
+        });
+
+        Scene scene = new Scene(grid, 400, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        Animal myAnimal = new Animal();
-        
-        new Fish();
-        Zebra myZebra = new Zebra();
-
-       
-        myAnimal.isMammal();
-        myAnimal.mate();
-
-        
-        myZebra.isMammal();
-        myZebra.mate();
-        myZebra.run();
+        launch(args);
     }
 }
 
-class Fish extends Animal {
-    private int sizeInFeet;
 
-    private void canEat() {
-        System.out.println("This is a private method canEat() from class Fish");
-    }
-
-    public int getSizeInFeet() {
-        return sizeInFeet;
-    }
-
-    public void setSizeInFeet(int sizeInFeet) {
-        this.sizeInFeet = sizeInFeet;
-    }
-
-    
-    public void feed() {
-        canEat();
-    }
-}
-
-class Zebra extends Animal {
-    public boolean is_wild;
-
-    public void run() {
-        System.out.println("This is the run method from class Zebra");
-    }
-}
